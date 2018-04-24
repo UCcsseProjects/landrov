5842f328a9c5d96b9f4239348ed637acffe311a2
Tim Rensen <tkr25@uclive.ac.nz>
2018-04-24 17:01:51 +1200

    added Arduino code and serial python script

---
+++ LandROVArduinoCode/LandROVArduinoCode.ino
@@ -0,0 +1,158 @@
+#include <Servo.h>
+#include <avr/wdt.h>
+
+#define DIST_PER_ENC_TICK 0.01
+
+#define LEFT_PWM_PIN 3
+#define LEFT_FW_PIN 4
+#define LEFT_RV_PIN 2
+#define RIGHT_PWM_PIN 5
+#define RIGHT_FW_PIN 7
+#define RIGHT_RV_PIN 6
+
+const byte LEFT_REAR_ENC_PIN_A = A0;
+const byte LEFT_REAR_ENC_PIN_B = A1;
+const byte LEFT_FRONT_ENC_PIN_A = A3;
+const byte LEFT_FRONT_ENC_PIN_B = A2;
+const byte RIGHT_REAR_ENC_PIN_A = 9;
+const byte RIGHT_REAR_ENC_PIN_B = 10;
+const byte RIGHT_FRONT_ENC_PIN_A = 11;
+const byte RIGHT_FRONT_ENC_PIN_B = 12;
+
+double distance_left;
+double distance_right;
+double velocity_left;
+double velocity_right;
+
+void setup() {
+  wdt_disable();
+  Serial.begin(115200);
+  
+  pinMode(LEFT_REAR_ENC_PIN_A, INPUT);
+  pinMode(LEFT_REAR_ENC_PIN_B, INPUT);
+  pinMode(LEFT_FRONT_ENC_PIN_A, INPUT);
+  pinMode(LEFT_FRONT_ENC_PIN_B, INPUT);
+  pinMode(RIGHT_REAR_ENC_PIN_A, INPUT);
+  pinMode(RIGHT_REAR_ENC_PIN_B, INPUT);
+  pinMode(RIGHT_FRONT_ENC_PIN_A, INPUT);
+  pinMode(RIGHT_FRONT_ENC_PIN_B, INPUT);
+  pinMode(LEFT_PWM_PIN, OUTPUT);
+  digitalWrite(LEFT_PWM_PIN, LOW);
+  pinMode(LEFT_FW_PIN, OUTPUT);
+  digitalWrite(LEFT_FW_PIN, LOW);
+  pinMode(LEFT_RV_PIN, OUTPUT);
+  digitalWrite(LEFT_RV_PIN, LOW);
+  pinMode(RIGHT_PWM_PIN, OUTPUT);
+  digitalWrite(RIGHT_PWM_PIN, LOW);
+  pinMode(RIGHT_FW_PIN, OUTPUT);
+  digitalWrite(RIGHT_FW_PIN, LOW);
+  pinMode(RIGHT_RV_PIN, OUTPUT);
+  digitalWrite(RIGHT_RV_PIN, LOW);
+
+  analogWrite(LEFT_PWM_PIN, 0);
+  analogWrite(RIGHT_PWM_PIN, 0);
+
+  cli();
+  *digitalPinToPCMSK(LEFT_REAR_ENC_PIN_A) |= bit (digitalPinToPCMSKbit(LEFT_REAR_ENC_PIN_A));  // enable pin
+  *digitalPinToPCMSK(RIGHT_REAR_ENC_PIN_A) |= bit (digitalPinToPCMSKbit(RIGHT_REAR_ENC_PIN_A));  // enable pin
+  PCIFR  |= bit (digitalPinToPCICRbit(LEFT_REAR_ENC_PIN_A)); // clear any outstanding interrupt
+  PCIFR  |= bit (digitalPinToPCICRbit(RIGHT_REAR_ENC_PIN_A)); // clear any outstanding interrupt
+  //PCICR  |= bit (digitalPinToPCICRbit(LEFT_REAR_ENC_PIN_A)); // enable interrupt for the group
+  //PCICR  |= bit (digitalPinToPCICRbit(RIGHT_REAR_ENC_PIN_A)); // enable interrupt for the group
+
+  sei();
+  wdt_enable(WDTO_2S);
+
+}
+
+void loop() {
+  double code;
+  int pwm_value;
+  
+  wdt_reset();
+
+  if (Serial.available())
+  {
+    code = (double) Serial.read();
+    delay(40);
+    if (code >= 128) {
+      //Left motor speed set: 128-255, 191 stationary
+      if (code > 191) {
+        pwm_value = (int) round(255*(code - 191)/64);
+        digitalWrite(LEFT_RV_PIN, LOW);
+        digitalWrite(LEFT_FW_PIN, HIGH);
+        analogWrite(LEFT_PWM_PIN, pwm_value);
+      } else if (code < 191) {
+        pwm_value = (int) round(255*(191 - code)/64);
+        digitalWrite(LEFT_FW_PIN, LOW);
+        digitalWrite(LEFT_RV_PIN, HIGH);
+        analogWrite(LEFT_PWM_PIN, pwm_value);
+      } else {
+        digitalWrite(LEFT_FW_PIN, LOW);
+        digitalWrite(LEFT_RV_PIN, LOW);
+        analogWrite(LEFT_PWM_PIN, 0);
+      }
+    } else {
+        //Right motor speed set: 0-127, 63 stationary
+        if (code > 63) {
+          Serial.print((char) code);
+          pwm_value = (int) round(255*(code - 63)/64);
+          Serial.print((char) pwm_value);
+          digitalWrite(RIGHT_RV_PIN, LOW);
+          digitalWrite(RIGHT_FW_PIN, HIGH);
+          analogWrite(RIGHT_PWM_PIN, pwm_value);
+        } else if (code < 63) {
+          Serial.print((char) code);
+          pwm_value = (int) round(255*(63 - code)/64);
+          Serial.print((char) pwm_value);
+          digitalWrite(RIGHT_FW_PIN, LOW);
+          digitalWrite(RIGHT_RV_PIN, HIGH);
+          analogWrite(RIGHT_PWM_PIN, pwm_value);
+        } else {
+          digitalWrite(RIGHT_FW_PIN, LOW);
+          digitalWrite(RIGHT_RV_PIN, LOW);
+          analogWrite(RIGHT_PWM_PIN, 0);
+        }
+    }
+  }
+      /*case 'D':
+        //Return track distance travelled
+        if (code_1 == 'R') Serial.println(distance_right);
+        else if (code_1 == 'L') Serial.println(distance_left);
+        else Serial.println("second char not recognised");
+        break;
+      case 'V':
+        //Return current track velocity
+        if (code_1 == 'R') Serial.println(velocity_right);
+        else if (code_1 == 'L') Serial.println(velocity_left);
+        else Serial.println("Second char not recognised");
+        break;
+      default:
+        Serial.println("Incorrect Input, drive speed: __ L/R 0-5-9, get distance: __ D R/L, get velocity: __ V R/L");*/
+}
+
+///////////////////////////////////////////////
+//  Left Encoder incrimenting/decrementing
+///////////////////////////////////////////////
+ISR (PCINT0_vect)
+{
+  cli();
+  // Interrupt on phase A pin
+  if (digitalRead(RIGHT_FRONT_ENC_PIN_A) == digitalRead(RIGHT_FRONT_ENC_PIN_B)) distance_right -= DIST_PER_ENC_TICK;
+  else distance_right += DIST_PER_ENC_TICK;
+  Serial.println("hello from right enc!");
+  sei();
+}
+
+///////////////////////////////////////////////
+//  Right Encoder incrimenting/decrementing
+///////////////////////////////////////////////
+ISR (PCINT1_vect)
+{
+  cli();
+  // Interrupt on phase A pin
+  if (digitalRead(LEFT_FRONT_ENC_PIN_A) == digitalRead(LEFT_FRONT_ENC_PIN_B)) distance_left -= DIST_PER_ENC_TICK;
+  else distance_left += DIST_PER_ENC_TICK;
+  Serial.println("hello from left enc!");
+  sei();
+}
---
+++ ser_control.py
@@ -0,0 +1,66 @@
+import serial
+import time
+from threading import Thread
+from threading import Lock
+
+serial_mutex = Lock()	# This mutex guards the left and right track speeds acessed by sepperate threads
+leftTrackVel = 0		#speed of left track [-1, 1]
+rightTrackVel = 0		#speed of right track [-1, 1]
+driverReturnValue = 0	#Return value from arduino, used for debug
+stop = False
+
+
+# Sends track speeds to arduino driver, prints recieved values
+def arduino_serial(ser):
+	while not stop:
+		time.sleep(0.1)
+		while ser.in_waiting:
+			driverReturnValue = ord(ser.read())
+			print(driverReturnValue)
+		serial_mutex.acquire()
+		left_value = int(round(191 + leftTrackVel*63))
+		print("Left value sent:", left_value)
+		ser.write(chr(left_value))
+		time.sleep(0.1)
+		right_value = int(round(63 + rightTrackVel*63))
+		ser.write(chr(right_value))
+		time.sleep(0.1)
+		serial_mutex.release()
+		
+
+if __name__ == '__main__':
+	# If running this on windows, 
+	# you will need to change the port to COM3 or similar, use device manager
+	ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+	time.sleep(4)
+	
+	try:
+		thread = Thread(target=arduino_serial, args=(ser,))
+		thread.start()
+        
+		# Hold position for 1 second
+		serial_mutex.acquire()
+		leftTrackVel = 0
+		rightTrackVel = 0
+		serial_mutex.release()
+		time.sleep(1)
+
+		# Move tracks forward at full speed
+		serial_mutex.acquire()
+		leftTrackVel = 0
+		rightTrackVel = -0.5
+		serial_mutex.release()
+		time.sleep(2)
+
+		# Hold position
+		serial_mutex.acquire()
+		leftTrackVel = 0
+		rightTrackVel = 0
+		serial_mutex.release()
+		time.sleep(1)
+	
+	finally:
+		print("Finishing")
+		stop = True
+		thread.join()
+		ser.close()
