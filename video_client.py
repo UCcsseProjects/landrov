796fc808c4a7fff9ba35022b8812268524912e1d
Ori <oga13@uclive.ac.nz>
2018-08-10 16:16:45 +1200

    update new IP

--- video_client.py
+++ video_client.py
@@ -6,20 +6,19 @@ import numpy as np
 port = "5557"
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://192.168.8.106:%s" % port)
-socket.setsockopt(zmq.SUBSCRIBE,b'rgbimage') 
+socket.connect("tcp://192.168.1.107:%s" % port)
+socket.setsockopt(zmq.SUBSCRIBE,b'rgbimage')
 print('connected to landrov server')
 
 
 
 while 1:
     k=cv2.waitKey(1)
-
-    if k!=-1:
-        if k  == 27 or k == ord('q'):
-            break
-        if socket.poll(0.001):
-            topic,msg = socket.recv_multipart()
-            if topic == b'rgbimage':
-                img = cv2.imdecode(np.fromstring(buf, dtype=np.uint8),cv2.IMREAD_COLOR)
-                cv2.imshow('img',img)
+    if k  == 27 or k == ord('q'):
+        break
+    if socket.poll(0.001):
+        topic,msg = socket.recv_multipart()
+        print('got topic',topic)
+        if topic == b'rgbimage':
+            img = cv2.imdecode(np.fromstring(msg, dtype=np.uint8),cv2.IMREAD_COLOR)
+            cv2.imshow('img',img)
